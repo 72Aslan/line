eda7c23865357eebf65e1a96626c0511aabc9a05
72Aslan <co127268@gmail.com>
2026-08-09 14:13:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# 確保 Vercel 能順利將根目錄與 api 目錄加入環境路徑
+sys.path.append(os.path.dirname(__file__))
+
 from api.webhook import app
 
-app = app
+# 讓 Vercel 能夠順利識別 WSGI 進入點
+application = app
